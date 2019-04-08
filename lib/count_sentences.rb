372d6392cbc2b58e19e0d7fb7f5f639aa1a7b944
Matthew Bartl <mattbartl@gmail.com>
2019-04-08 01:23:07 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,7 +16,7 @@ class String
 
   def count_sentences
     binding.pry
-    array = self.split(".")
+    array = self.split /[.!?]/
     array.map! {|item| item.split("?")}
     array.map! {|item| item.split("!")}
     array.length
